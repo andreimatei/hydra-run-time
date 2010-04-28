5a52f9da1424ea44af6e934ac08981a9f00ade33
kena <kena@e97e5017-a994-416e-809d-76780e9f78db>
2010-04-28 23:52:14 +0000

    [sl-tests] Update copyright notice of files changed in 2010.
    * 0_slc/1_createargs/useexit2.c,
    * 2_lib/assert.c,
    * 2_lib/clock.c,
    * 2_lib/div/div.c,
    * 2_lib/div/div2.c,
    * 2_lib/malloc/m1.c,
    * 2_lib/malloc/m2.c,
    * 2_lib/perf.c,
    * 2_lib/sac1.c: Add 2010 to copyright notice.
    
    git-svn-id: svn+ssh://mac-chris/Library/SVN/CSA/sw/svp/sys/trunk@3671 e97e5017-a994-416e-809d-76780e9f78db

--- tests/2_lib/malloc/m2.c
+++ tests/2_lib/malloc/m2.c
@@ -1,7 +1,7 @@
 //
 // m2.c: this file is part of the SL toolchain.
 //
-// Copyright (C) 2009 Universiteit van Amsterdam.
+// Copyright (C) 2009,2010 Universiteit van Amsterdam.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
